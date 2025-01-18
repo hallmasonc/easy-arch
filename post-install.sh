b6ba7434a2504fa769d7283155a4a2e72c66e0a4
Mason Hall <71787606+hallmasonc@users.noreply.github.com>
2025-01-18 02:33:15 -0500

    Made tweaks in post-install.sh for flatpak and fixed case-sensitvity in flatpak.txt.

--- post-install.sh
+++ post-install.sh
@@ -2,6 +2,7 @@
 
 # variables
 yay="https://aur.archlinux.org/yay.git"
+yayDir="./yay"
 
 # function
 cloneFunction () {
@@ -10,6 +11,26 @@ cloneFunction () {
 
 # Install yay as AUR helper
 cloneFunction $yay
-cd yay
-ls -la ./
-#makepkg -si
+
+if [ ! -d "$yayDir" ]; then
+    echo "failed to clone yay repo"
+else
+    # move and build
+    cd $yayDir
+    ls -la ./
+    #makepkg -si
+
+    # Check yay version
+    yay --version
+
+    # Return to parent directory
+    cd ../
+fi
+
+echo "$(pwd)"
+
+# Add flathub as remote for flatpaks
+flatpak remote-add flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Install flatpaks
+xargs flatpak --user install -y < ./flatpak.txt
